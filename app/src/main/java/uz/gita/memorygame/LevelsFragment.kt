e39b691323dae1dda07fa4f3bd8c5359ec77627d
behruzbek2Gulomov <120156394+behruzbek2Gulomov@users.noreply.github.com>
2023-10-02 21:04:56 +0500

    first commit

---
+++ app/src/main/java/uz/gita/memorygame/LevelsFragment.kt
@@ -0,0 +1,49 @@
+package uz.gita.memorygame
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.gita.memorygame.databinding.FragmentLevelsBinding
+
+private const val ARG_PARAM1 = "param1"
+private var _binding: FragmentLevelsBinding? =null
+private val binding get() =_binding!!
+class LevelsFragment : Fragment() {
+    private var param1: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getInt(ARG_PARAM1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        loadUI(param1!!)
+        return inflater.inflate(R.layout.fragment_levels, container, false)
+    }
+    fun loadUI(level: Int){
+        binding.mainLinear.weightSum=level.toFloat()
+        for (i in 0..level){
+            var innerLayout = binding.mainLinear.getChildAt(i)
+
+        }
+
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance(level: Int) =
+            LevelsFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_PARAM1, level)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/uz/gita/memorygame/model/Levels.kt
@@ -0,0 +1,5 @@
+package uz.gita.memorygame.model
+
+enum class Levels(var level:Int) {
+    EASY(5),MIDDLE(6),HARD(7)
+}
